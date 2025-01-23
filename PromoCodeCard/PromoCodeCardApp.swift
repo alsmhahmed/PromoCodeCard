60ecba9ab2ea466d104ae931b27c493a444598a6
alsmhahmed <alsmh.ahmed@gen-c.com>
2025-01-23 17:23:49 +0200

    Initial Commit

---
+++ PromoCodeCard/PromoCodeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromoCodeCardApp.swift
+//  PromoCodeCard
+//
+//  Created by alsmh ahmed on 23/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PromoCodeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
